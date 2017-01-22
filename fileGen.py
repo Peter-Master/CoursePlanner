a4a2c96e02d7fcae70a630c42d2bcec736f979c1
Peter Master <petehoke@comcast.net>
2017-01-21 18:35:16 -0800

    update documentation for fileGen.py

--- fileGen.py
+++ fileGen.py
@@ -1,12 +1,40 @@
+# by Peter Master, Alex Vossmeyer
+# 1/21/17
+#
+#
+# ASSUMPTIONS:
+#
+# 1) 
+#
+#
+#
+# BEHAVIOR:
+#
+# 1) Area A1 and Area A2 courses count towards only A1 and A2 respectively,
+# they do not count towards any other categories or requirements. Thus, we leave
+# A1 and A2 off from our program as the user can select from the registrar's A1 and
+# A2 lists completely individually of the rest of their requirements.
+#
+# 2) A stipulation from the registrar: "Please note that one course may only count
+# toward one A‐G requirement but that A‐G courses may overlap with other Special
+# Subject Requirements". Thus, for a user who needs at least one more course in each
+# of areas D and E, a course that can cover either of area D and area E will 
+# only have its fill ranking benefited by 1 rather than 2.
+#
+
+
+
 import re
 
-areaAFile = open("a.txt")
-areaAList = aFile.read()
-newAreaAList = areaAList.split("\n")
+
 
 areaBFile = open("b.txt")
 areaBList = bFile.read()
 newAreaBList = areaBList.split("\n")
+
+areaCFile = open("c.txt")
+areaCList = cFile.read()
+newAreaCList = areaCList.split("\n")
 
 culturesFile = open("cultures.txt")
 culturesList = culturesFile.read()
